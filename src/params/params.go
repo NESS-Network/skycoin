92d3ac495a3da5e03e1865fa322998a2138efae1
Jeff-Bouchard <jeff-bouchard@protonmail.com>
2020-12-04 19:38:28 +0000

    privateness rebase

--- src/params/params.go
+++ src/params/params.go
@@ -8,8 +8,8 @@ AVOID EDITING THIS MANUALLY
 var (
 	// MainNetDistribution Skycoin mainnet coin distribution parameters
 	MainNetDistribution = Distribution{
-		MaxCoinSupply:        100000000,
-		InitialUnlockedCount: 25,
+		MaxCoinSupply:        200000000,
+		InitialUnlockedCount: 10,
 		UnlockAddressRate:    5,
 		UnlockTimeInterval:   31536000,
 		Addresses: []string{
@@ -119,7 +119,7 @@ var (
 	// UserVerifyTxn transaction verification parameters for user-created transactions
 	UserVerifyTxn = VerifyTxn{
 		// BurnFactor can be overriden with `USER_BURN_FACTOR` env var
-		BurnFactor: 10,
+		BurnFactor: 1,
 		// MaxTransactionSize can be overriden with `USER_MAX_TXN_SIZE` env var
 		MaxTransactionSize: 32768, // in bytes
 		// MaxDropletPrecision can be overriden with `USER_MAX_DECIMALS` env var
