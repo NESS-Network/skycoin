cfa5fdb64fbf2ced48c7535ca6604c90a42045f2
root <root@packer-output-ea516ee3-01ab-4739-9b91-fe99a311d5bf>
2021-05-04 09:04:17 +0000

    fix

--- run-daemon.sh
+++ run-daemon.sh
@@ -16,11 +16,11 @@ GORUNFLAGS=${GORUNFLAGS:-}
 export USER_BURN_FACTOR=4
 go run -ldflags "${GOLDFLAGS}" $GORUNFLAGS cmd/privateness/privateness.go \
     -gui-dir="${DIR}/src/gui/static/" \
-    -max-default-peer-outgoing-connections=7 \
-    -launch-browser=true \
-    -enable-all-api-sets=true \
+    -max-default-peer-outgoing-connections=21 \
+    -launch-browser=false \
+    -enable-all-api-sets=false \
     -enable-gui=false \
-    -log-level=debug \
+    -log-level=info \
     $@
 
 popd >/dev/null
